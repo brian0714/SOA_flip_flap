3e631d63816a58a1aef5f185ba40157b92f00147
Brian <brian2000890714@gmail.com>
2023-10-06 01:55:40 +0800

    All tests pass; rubocop/flog resolved

--- tsv_buddy.rb
+++ tsv_buddy.rb
@@ -6,13 +6,20 @@ module TsvBuddy
   # arguments: tsv - a String in TSV format
   # returns: nothing
   def take_tsv(tsv)
-
+    # puts tsv.class
+    # puts tsv
+    tsv_data = tsv.split("\n")
+    header, *data = tsv_data.map { |row| row.split("\t") }
+    @data = data.map { |row| header.zip(row).to_h }
   end
 
   # Converts @data into tsv string
   # arguments: none
   # returns: String in TSV format
   def to_tsv
-
+    tsv = []
+    tsv << @data[0].keys.join("\t")
+    @data.each{ |item| tsv << item.values.join("\t") }
+    "#{tsv.join("\n")}\n"
   end
 end
